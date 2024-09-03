aed26f82c001fb762275fe9e10058a69c0131e62
Niels Liebisch <niels@heyhosystems.com>
2024-09-03 11:57:51 +0200

    execute google tag manager right away

--- app/javascript/google_tag_manager.js
+++ app/javascript/google_tag_manager.js
@@ -1,4 +1,4 @@
-window.addEventListener("load", function(){
+window.addEventListener("load", function () {
     window.cookieconsent.initialise({
         "palette": {
             "popup": {
@@ -15,24 +15,22 @@ window.addEventListener("load", function(){
             "link": "Learn more",
             "href": "https://heyhosystems.com/impressum/"  // Link to your privacy policy page
         },
-        "onStatusChange": function(status) {
+        "onStatusChange": function (status) {
             if (status === 'allow') {
                 // If cookies are allowed, load Google Analytics, Ads, and Tag Manager scripts
                 // loadGoogleAnalytics();
                 // loadGoogleAds();
-                loadGoogleTagManager();
+                // loadGoogleTagManager();
             }
         }
     });
 });
 
-function loadGoogleTagManager() {
-    (function (w, d, s, l, i) {
-        w[l] = w[l] || []; w[l].push({
-            'gtm.start':
-                new Date().getTime(), event: 'gtm.js'
-        }); var f = d.getElementsByTagName(s)[0],
-            j = d.createElement(s), dl = l != 'dataLayer' ? '&l=' + l : ''; j.async = true; j.src =
-                'https://www.googletagmanager.com/gtm.js?id=' + i + dl; f.parentNode.insertBefore(j, f);
-    })(window, document, 'script', 'dataLayer', 'GTM-KDKLVKTM');
-}
+(function (w, d, s, l, i) {
+    w[l] = w[l] || []; w[l].push({
+        'gtm.start':
+            new Date().getTime(), event: 'gtm.js'
+    }); var f = d.getElementsByTagName(s)[0],
+        j = d.createElement(s), dl = l != 'dataLayer' ? '&l=' + l : ''; j.async = true; j.src =
+            'https://www.googletagmanager.com/gtm.js?id=' + i + dl; f.parentNode.insertBefore(j, f);
+})(window, document, 'script', 'dataLayer', 'GTM-KDKLVKTM');
